bf3fccd380a8d33a64b5251d661a0318a7171071
PatrykMazurek <33990246+PatrykMazurek@users.noreply.github.com>
2023-12-02 14:04:33 +0100

    Initial commit

---
+++ src/main/java/pl/uken/asi1_n/Asi1_nApplication.java
@@ -0,0 +1,12 @@
+package pl.uken.asi1_n;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Asi1_nApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Asi1_nApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/uken/asi1_n/Asi1_nApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.uken.asi1_n;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Asi1_nApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
